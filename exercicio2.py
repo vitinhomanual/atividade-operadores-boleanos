13ff6f77b405ef3f5003eabbf0b391fb5f4dc480
Vitor <Vitor@ex.com>
2025-09-16 09:33:09 -0300

    exencicio 2

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+idade=int(input("digite sua iadade"))
+Atestado=(input("você tem atestado?(sim)"))
+if idade >= 18 and Atestado =="8":
+    print("pode se inscrever no clube")
+else:
+    print("nao pode se inscrever")
